a7a619da820c4c414539115e3d2bf3bfff9a61ff
shourya <shouryaraj1999@gmail.com>
2020-08-16 22:13:46 +1000

    removed gitignore file

---
+++ venv/Scripts/pip-script.py
@@ -0,0 +1,12 @@
+#!"E:\UNITS\Year_3\Semester 2\FIT3080\Artificial-Intelligence-Algorithms\venv\Scripts\python.exe"
+# EASY-INSTALL-ENTRY-SCRIPT: 'pip==10.0.1','console_scripts','pip'
+__requires__ = 'pip==10.0.1'
+import re
+import sys
+from pkg_resources import load_entry_point
+
+if __name__ == '__main__':
+    sys.argv[0] = re.sub(r'(-script\.pyw?|\.exe)?$', '', sys.argv[0])
+    sys.exit(
+        load_entry_point('pip==10.0.1', 'console_scripts', 'pip')()
+    )
